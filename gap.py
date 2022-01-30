1e9860df8b69d89a5d57bce7027fd788a34397a5
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-01-30 13:52:55 -0600

    selection

--- gap.py
+++ gap.py
@@ -1,4 +1,5 @@
 from string import punctuation
+from this import s
 import numpy as np
 import math
 import random
@@ -13,23 +14,51 @@ def population(n_ind, lim_inf, lim_sup):
     for i in range(n_ind):
         P[i,0] = random.uniform(lim_inf, lim_sup) #componet x of vector i
         P[i,1] = random.uniform(lim_inf, lim_sup) #componet y of vector i
-    return P
+    return P #matrix of pupulation
 
+#fitness from one individual
 def fitness(x, y):
-    return 1/(1+F(x,y))
+    return 1/(1+F(x,y)) #if an individual's fitness approaches 1 is a good candidate  
 
+#fitness from all individuals
 def fitness_all(n_ind):
-    P = population(n_ind,1,3)
+    P = population(n_ind,-6,6)
     listfitness = []
     for i in range(n_ind):
         listfitness.append(fitness(P[i,0],P[i,1]))
-    return listfitness
+    return listfitness, P
 
+#selection.1
+def selection1(n_ind):
+    L, P = fitness_all(n_ind)
+    max_value = max(L)
+    s = []
+    for i in range(len(L)):
+        p = L[i]/max_value
+        if p >= 0.3:
+           s.append(P[i,0])
+           s.append(P[i,1])
+    return s
+
+#selection.2
+def selection2(n_ind):
+    s = selection1(n_ind)
+    row = int(len(s)/2)
+    S = np.zeros((row,2))
+    k=0
+    for j in range(row):
+        S[j,0]=s[2*j]
+        S[j,1]=s[j+k+1]
+        k=k+1
+    return S
 
 def run():
     n_ind = int(input("Numero de individuos: "))
-    print(population(n_ind,1,3))
-    P = population(n_ind,1,3)
-    print(fitness_all(n_ind))
+    S = selection2(n_ind)
+
+    print(S)
+    
+
+
 if __name__ == "__main__":
     run()
