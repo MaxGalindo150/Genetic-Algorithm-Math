47bade77e3ede490273d354ceaa23e1f8dcfcbf2
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-01-30 00:46:43 -0600

    get fitness from all individuals

--- gap.py
+++ gap.py
@@ -1,3 +1,4 @@
+from string import punctuation
 import numpy as np
 import math
 import random
@@ -10,14 +11,25 @@ def F(x,y):
 def population(n_ind, lim_inf, lim_sup):
     P = np.zeros((n_ind,2))
     for i in range(n_ind):
-        P[i,0] = random.uniform(lim_inf, lim_sup) #compotente x del vector i
-        P[i,1] = random.uniform(lim_inf, lim_sup) #compotente y del vector i
+        P[i,0] = random.uniform(lim_inf, lim_sup) #componet x of vector i
+        P[i,1] = random.uniform(lim_inf, lim_sup) #componet y of vector i
     return P
+
+def fitness(x, y):
+    return 1/(1+F(x,y))
+
+def fitness_all(n_ind):
+    P = population(n_ind,1,3)
+    listfitness = []
+    for i in range(n_ind):
+        listfitness.append(fitness(P[i,0],P[i,1]))
+    return listfitness
 
 
 def run():
     n_ind = int(input("Numero de individuos: "))
     print(population(n_ind,1,3))
-
+    P = population(n_ind,1,3)
+    print(fitness_all(n_ind))
 if __name__ == "__main__":
     run()
