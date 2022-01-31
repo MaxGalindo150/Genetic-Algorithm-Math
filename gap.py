c5d37b87d7f989745fd5e14d6cfaa95c79032139
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-01-31 14:49:21 -0600

    the functions works

--- gap.py
+++ gap.py
@@ -1,11 +1,8 @@
-from re import M, S
-import re
 from string import punctuation
-from this import s
-from tkinter import E
 import numpy as np
 import math
 import random
+from scipy.stats import norm 
 
 #function to optimize 
 def F(x,y):
@@ -44,7 +41,7 @@ def selection1(n_ind):
     return s
 
 #selection.2
-def selection2(n_ind):
+def selection2(n_ind): #papas
     s = selection1(n_ind)
     row = int(len(s)/2)
     S = np.zeros((row,2))
@@ -58,30 +55,80 @@ def selection2(n_ind):
 #crossover1
 def crossover1(n_ind):
     S = selection2(n_ind)
-    St = []
+    LS = []
+    LSn = []
     for i in range(len(S)):
-        St.append(str(S[i,0]))
-        St.append(str(S[i,1]))
-    return St
+        LS.append(str(int(10000*S[i,0])))
+        LS.append(str(int(10000*S[i,1])))
+        LSn.append(S[i,0])
+        LSn.append(S[i,1])
+    return LS, LSn
 
 #crossover2
 def crossover2(n_ind):
-    P = crossover1(n_ind)
+    LS, LSn = crossover1(n_ind)
     H = []
     k1 = 0 
     k2 = 0
-    for i in range(int(len(P)/2)):
+    for i in range(int(len(LS)/2)):
         n = random.randint(0,4)
-        H.append(float(P[i+k2][0:n]+P[2*(i+1) + k1][n:len(P[1])]))
-        H.append(float(P[i+1+k2][0:n]+P[2*(i+1)+1+k1][n:len(P[1])]))
-        k1 = k1-4
-        k2 = k2+1
-    H.pop()    
-    H.pop()
-    return P, H
+        if len(LS) == 2:
+            k1 = -1
+            H.append(float(LS[i+k2][0:n]+LS[2*(i+1) + k1][n:len(LS[2*(i+1) + k1])]))
+        else:    
+            H.append(float(LS[i+k2][0:n]+LS[2*(i+1) + k1][n:len(LS[2*(i+1) + k1])]))
+            H.append(float(LS[i+1+k2][0:n]+LS[2*(i+1)+1+k1][n:len(LS[2*(i+1)+1+k1])]))
+            k1 = k1-4
+            k2 = k2+1  
+    if len(LS) == 2:
+        pass
+    else:
+        H.pop()      
+        H.pop()
+    return H
+
+#mutation
+def mutation(n_ind):
+    H = crossover2(n_ind)
+    MT = []
+    for i in range(len(H)):
+        x = random.uniform(-1, 1)
+        mut1 = 10000*round(norm.pdf(x, 1, 1),4)
+        MT.append(round((H[i] + mut1)/10000,4))
+    return MT
+
+#family integration 
+def integration(n_ind):
+    LS, LSn = crossover1(n_ind)
+    MT = mutation(n_ind)
+    return LSn + MT
+
+#family matrix
+def mf(n_ind):
+    F = integration(n_ind)
+    MF = np.zeros((int(len(F)/2),2))
+    k=0
+    for j in range(int(len(F)/2)):
+        MF[j,0]=F[2*j]
+        MF[j,1]=F[j+k+1]
+        k=k+1
+    return MF
 
 
-#convert to binary
+
+def run():
+    n_ind = int(input("Numero de individuos: "))
+
+
+    
+
+
+if __name__ == "__main__":
+    run()
+
+
+
+ #convert to binary
 #def convertbin(n_ind):
  #   S = selection2(n_ind)
   #  E = []
@@ -104,13 +151,19 @@ def crossover2(n_ind):
         #k2 = k2+1
     #F = P + H
     #return H, P, F   
-
-
-def run():
-    n_ind = int(input("Numero de individuos: "))
-    P, H = crossover2(n_ind)
-    print(P)
-    print()
-    print(H)   
-if __name__ == "__main__":
-    run()
+   
+   #k=0
+    #if len(H) == 1:
+     #   x1 = random.uniform(-1, 1)
+      #  mut = 10000*round(norm.pdf(x1, 1, 1),4)
+       # H = (H + mut)/10000
+        #MT = H
+    #else:    
+     #   for i in range(int(len(H)/2)):
+      #      x = random.uniform(-1, 1)
+       #     mut1 = 10000*round(norm.pdf(x, 1, 1),4)
+        #    mut2 = 10000*round(norm.pdf(x, 1, 1),4)
+         #   M[i,0]=round((H[2*i] + mut1)/10000,4)
+          #  M[i,1]=round((H[i+k+1] + mut2)/10000,4)
+           # k=k+1
+        #MT = M    
