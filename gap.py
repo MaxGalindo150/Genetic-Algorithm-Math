0e38934972714153b99cd5c04462ce3a4d912416
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-01-31 16:20:23 -0600

    the skeleton of the algorithm is complete

--- gap.py
+++ gap.py
@@ -21,96 +21,70 @@ def fitness(x, y):
     return round(1/(1+F(x,y)),4) #if an individual's fitness approaches 1 is a good candidate  
 
 #fitness from all individuals
-def fitness_all(n_ind):
-    P = population(n_ind,-6,6)
+def fitness_all(P_matrix):
     listfitness = []
-    for i in range(n_ind):
-        listfitness.append(fitness(P[i,0],P[i,1]))
-    return listfitness, P
+    for i in range(len(P_matrix)):
+        listfitness.append(fitness(P_matrix[i,0],P_matrix[i,1]))
+    return listfitness
 
-#selection.1
-def selection1(n_ind):
-    L, P = fitness_all(n_ind)
-    max_value = max(L)
-    s = []
-    for i in range(len(L)):
-        p = L[i]/max_value
+#selection
+def selection(listfitness, P_matrix):
+    max_value = max(listfitness)
+    points_survivorsC = []
+    points_survivors = []
+    for i in range(len(listfitness)):
+        p = listfitness[i]/max_value
         if p >= 0.3:
-           s.append(P[i,0])
-           s.append(P[i,1])
-    return s
+           points_survivorsC.append(str(int(10000*P_matrix[i,0])))
+           points_survivorsC.append(str(int(10000*P_matrix[i,1])))
+           points_survivors.append(P_matrix[i,0])
+           points_survivors.append(P_matrix[i,1])
+    return points_survivorsC, points_survivors #surviving vectors are placed on a list 
 
-#selection.2
-def selection2(n_ind): #papas
-    s = selection1(n_ind)
-    row = int(len(s)/2)
-    S = np.zeros((row,2))
-    k=0
-    for j in range(row):
-        S[j,0]=s[2*j]
-        S[j,1]=s[j+k+1]
-        k=k+1
-    return S
 
-#crossover1
-def crossover1(n_ind):
-    S = selection2(n_ind)
-    LS = []
-    LSn = []
-    for i in range(len(S)):
-        LS.append(str(int(10000*S[i,0])))
-        LS.append(str(int(10000*S[i,1])))
-        LSn.append(S[i,0])
-        LSn.append(S[i,1])
-    return LS, LSn
-
-#crossover2
-def crossover2(n_ind):
-    LS, LSn = crossover1(n_ind)
-    H = []
+#crossover
+def crossover(points_survivorC):
+    offspring = []
     k1 = 0 
     k2 = 0
-    for i in range(int(len(LS)/2)):
+    for i in range(int(len(points_survivorC)/2)):
         n = random.randint(0,4)
-        if len(LS) == 2:
+        if len(points_survivorC) == 2:
             k1 = -1
-            H.append(float(LS[i+k2][0:n]+LS[2*(i+1) + k1][n:len(LS[2*(i+1) + k1])]))
+            offspring.append(float(points_survivorC[i+k2][0:n]+points_survivorC[2*(i+1) + k1][n:len(points_survivorC[2*(i+1) + k1])]))
         else:    
-            H.append(float(LS[i+k2][0:n]+LS[2*(i+1) + k1][n:len(LS[2*(i+1) + k1])]))
-            H.append(float(LS[i+1+k2][0:n]+LS[2*(i+1)+1+k1][n:len(LS[2*(i+1)+1+k1])]))
+            offspring.append(float(points_survivorC[i+k2][0:n]+points_survivorC[2*(i+1) + k1][n:len(points_survivorC[2*(i+1) + k1])]))
+            offspring.append(float(points_survivorC[i+1+k2][0:n]+points_survivorC[2*(i+1)+1+k1][n:len(points_survivorC[2*(i+1)+1+k1])]))
             k1 = k1-4
             k2 = k2+1  
-    if len(LS) == 2:
+    if len(points_survivorC) == 2:
         pass
     else:
-        H.pop()      
-        H.pop()
-    return H
+        offspring.pop()      
+        offspring.pop()
+    return offspring
 
 #mutation
-def mutation(n_ind):
-    H = crossover2(n_ind)
-    MT = []
-    for i in range(len(H)):
+def mutation(offspring):
+    offspringM = []
+    for i in range(len(offspring)):
         x = random.uniform(-1, 1)
         mut1 = 10000*round(norm.pdf(x, 1, 1),4)
-        MT.append(round((H[i] + mut1)/10000,4))
-    return MT
+        offspringM.append(round((offspring[i] + mut1)/10000,4))
+    return offspringM
 
 #family integration 
-def integration(n_ind):
-    LS, LSn = crossover1(n_ind)
-    MT = mutation(n_ind)
-    return LSn + MT
+def integration(points_survivors, offspringM):
+    family = points_survivors + offspringM
+    return family 
 
 #family matrix
-def mf(n_ind):
-    F = integration(n_ind)
-    MF = np.zeros((int(len(F)/2),2))
+def mf(family):
+    MF = np.zeros((int(len(family)/2),2))
     k=0
-    for j in range(int(len(F)/2)):
-        MF[j,0]=F[2*j]
-        MF[j,1]=F[j+k+1]
+    for j in range(int(len(family)/2)):
+        MF[j,0]=family[2*j]
+        MF[j,1]=family[j+k+1]
         k=k+1
     return MF
 
@@ -118,8 +92,41 @@ def mf(n_ind):
 
 def run():
     n_ind = int(input("Numero de individuos: "))
-
-
+    lim_inf = float(input("Limite inferior de x y y: "))
+    lim_sup = float(input("Limite superior de x y y: "))
+    P = population(n_ind, lim_inf, lim_sup)
+    print("Matriz inicial:")
+    print(P)
+    print()
+    print("Fitness de los vectores padre:")
+    listfitness = fitness_all(P)
+    print(listfitness)
+    print()
+    print("Mejores fitness:")
+    points_survivorsC, points_survivors = selection(listfitness, P)
+    print()
+    print(" Vectores seleccionados:")
+    print()
+    print(points_survivors)
+    print()
+    print(" Vectores seleccionados y preparados para cruzar:")
+    print(points_survivorsC)
+    print()
+    print("cruce:")
+    offspring = crossover(points_survivorsC)
+    print(offspring)
+    print()
+    print("Mutacion:")
+    offspringM = mutation(offspring)
+    print(offspringM)
+    print()
+    print("juntamos padres e hijos:")
+    family = integration(points_survivors, offspringM)
+    print(family)
+    print()
+    print("hacemos la matriz de la familia:")
+    MF = mf(family)
+    print(MF)
     
 
 
@@ -167,3 +174,28 @@ if __name__ == "__main__":
           #  M[i,1]=round((H[i+k+1] + mut2)/10000,4)
            # k=k+1
         #MT = M    
+
+   #selection.2
+#def selection2(n_ind): #papas
+ #   s = selection1(n_ind)
+  #  row = int(len(s)/2)
+   # S = np.zeros((row,2))
+    #k=0
+    #for j in range(row):
+     #   S[j,0]=s[2*j]
+      #  S[j,1]=s[j+k+1]
+       # k=k+1
+    #return S
+
+#crossover1
+#def crossover1(n_ind):
+ #   S = selection2(n_ind)
+  #  LS = []
+   # LSn = []
+    #for i in range(len(S)):
+     #   LS.append(str(int(10000*S[i,0])))
+      #  LS.append(str(int(10000*S[i,1])))
+       # LSn.append(S[i,0])
+        #LSn.append(S[i,1])
+    #return LS, LSn
+     
